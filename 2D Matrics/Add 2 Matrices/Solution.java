d0880cde2036e96e33904906b6572e324f44cc11
Arjun1713 <arjunnigam.1713@gmail.com>
2025-08-27 13:27:34 +0530

    Adding Two Matrices
    Taking two matrices of same dimensions and adding them together.

---
+++ 2D Matrics/Add 2 Matrices/Solution.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int[][] a = new int[n][m];
+		int[][] b = new int[n][m];
+		
+		for(int i=0;i<n;i++)         // input for first matrix
+		{
+		    for(int j = 0;j<m;j++)
+		    {
+		        a[i][j] = sc.nextInt();
+		    }
+		    
+		}
+		
+		
+		for(int i=0;i<n;i++)        // input for second matrix
+		{
+		    for(int j = 0;j<m;j++)
+		    {
+		        b[i][j] = sc.nextInt();
+		    }
+		    
+		}
+		
+		for(int i=0;i<n;i++)        // Adding two matrices
+		{
+		    for(int j = 0;j<m;j++)
+		    {
+		        System.out.print(b[i][j] + a[i][j] + " ");
+		    }
+		    System.out.println();
+		    
+		}
+		
+
+		
+		
+		
+
+	}
+}
